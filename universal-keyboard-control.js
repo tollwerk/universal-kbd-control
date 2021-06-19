a9f3d3347532158bf77d0e254d8441281344a7b9
Joschi Kuphal <joschi@tollwerk.de>
2021-06-19 13:52:29 +0200

    Add demo page

--- universal-keyboard-control.js
+++ universal-keyboard-control.js
@@ -1,3 +1,16 @@
 (function (w, d) {
-    alert('install');
+    let kbdactive = false;
+
+    const kbdbar = d.createElement('footer');
+    kbdbar.className = 'ukbdc-bar';
+    const kbdbtn = d.createElement('button');
+    kbdbtn.appendChild(d.createTextNode('Gyroskopische Steuerung'))
+    kbdbtn.className = 'ukbdc-btn';
+    kbdbtn.setAttribute('aria-pressed', 'false');
+    kbdbtn.addEventListener('click', e => {
+        kbdactive = !kbdactive;
+        kbdbtn.setAttribute('aria-pressed', kbdactive ? 'true' : 'false');
+    });
+    kbdbar.appendChild(kbdbtn);
+    d.body.appendChild(kbdbar);
 })(window, document)
